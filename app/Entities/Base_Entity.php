a779d5ebc1d56a28e01ddb9df621fda22a48e0f1
Mai ZhiKun <maizk@haplox.com>
2016-02-28 23:29:54 +0800

    first commit

---
+++ app/Entities/Base_Entity.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maizhikun
+ * Date: 16/2/28
+ * Time: 下午10:45
+ */
+namespace App\Entities;
+/**
+ * Created by PhpStorm.
+ * User: maizhikun
+ * Date: 16/2/28
+ * Time: 下午10:42
+ */
+class Base_Entity{
+
+}
---
+++ app/Entities/User_Entity.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Entities;
+/**
+ * Created by PhpStorm.
+ * User: maizhikun
+ * Date: 16/2/28
+ * Time: 下午10:42
+ */
+class User_Entity extends Base_Entity{
+
+    public $entity = [
+        "User" => "用户"
+    ];
+
+    
+}
