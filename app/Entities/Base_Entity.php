7033ba4f874e043bbe4a4bd1a72cf5ca16f94e06
Mai ZhiKun <maizk@haplox.com>
2016-03-02 21:36:52 +0800

    属性设置完成

--- app/Entities/Base_Entity.php
+++ app/Entities/Base_Entity.php
@@ -6,12 +6,35 @@
  * Time: 下午10:45
  */
 namespace App\Entities;
-/**
- * Created by PhpStorm.
- * User: maizhikun
- * Date: 16/2/28
- * Time: 下午10:42
- */
+
 class Base_Entity{
+
+    private $base_attribute = array();
+
+    public $attribute = array();
+
+    public function get_base_attribute(){
+        $this->base_attribute['id'] = $this->set_attribute("序号","id");
+        $this->base_attribute['created_at'] = $this->set_attribute("创建时间","date_time");
+        $this->base_attribute['updated_at'] = $this->set_attribute("修改时间","date_time");
+        $this->base_attribute['deleted_at'] = $this->set_attribute("删除时间","date_time");
+        return $this->base_attribute ;
+    }
+
+    /**
+     * @param $front 前端显示字段名
+     * @param $type 字段类型 "id" , "string" , "date_time" "picture"
+     * @param $option 选项
+     * @param $length 字段长度
+     * @return array
+     */
+    public static function  set_attribute($front , $type , $option = [] ,$length = 255){
+        return $attribute = [
+            "front" => $front,
+            "type" => $type,
+            "option" => $option,
+            "length" => $length,
+        ];
+    }
 
 }
--- app/Entities/User_Entity.php
+++ app/Entities/User_Entity.php
@@ -13,5 +13,7 @@ class User_Entity extends Base_Entity{
         "User" => "用户"
     ];
 
+
+
     
 }
